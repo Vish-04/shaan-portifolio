6dcf1bf710526a3add2c426ec248b292d5e3aeed
VishReddy <vishwareddy@gmail.com>
2024-09-05 15:34:39 -0400

    minor changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,37 @@
 import Image from "next/image";
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Link from 'next/link';
 import { FaLinkedin, FaEnvelope, FaPhone } from 'react-icons/fa';
 
 export default function WorkWithMeSection() {
   return (
-    <Box className="flex flex-col items-center justify-center min-h-screen w-screen overflow-y-auto">
-      <h2>Work With Me</h2>
-      <Image src="/images/p2_img.jpg" alt="Profile Picture" width={200} height={200} />
-      <Box className="flex flex-col items-start">
-        <Box className="flex items-center">
-          <FaLinkedin size={24} />
-          <Link href="https://linkedin.com/in/jain-shaan">LinkedIn</Link>
+    <Box className="flex items-center justify-evenly min-h-screen w-screen overflow-y-auto p-8">
+      <Typography variant="h1" className="w-[500px] text-center">Work With Me</Typography>
+      <Image src="/images/p2_img.jpg" alt="Profile Picture" width={300} height={300} />
+
+      <Box className="flex flex-col h-max mt-auto mb-auto items-start">
+        <Box className="w-full self-end bg-gradient-to-r h-0.5 from-purple-400 via-blue-500 to-green-500 mb-8" />
+        <Box className="flex items-center mb-4">
+          <FaLinkedin size={48} />
+          <Box>
+            <Typography className="text-xl ml-2">LinkedIn</Typography>
+            <Link href="https://linkedin.com/in/jain-shaan" className="text-xl ml-2 text-blue-400 hover:text-blue-500 hover:underline transition-all duration-300" >https://linkedin.com/in/jain-shaan</Link>
+          </Box>
         </Box>
-        <Box className="flex items-center">
-          <FaEnvelope size={24} />
-          <Link href="mailto:shaanjain00@gmail.com">shaanjain00@gmail.com</Link>
+        <Box className="flex items-center mb-4">
+          <FaEnvelope size={48} />
+          <Box>
+            <Typography className="text-xl ml-2">Email</Typography>
+            <Link href="mailto:shaanjain00@gmail.com" className="text-xl ml-2 text-blue-400 hover:text-blue-500 hover:underline transition-all duration-300" >shaanjain00@gmail.com</Link>
+          </Box>
         </Box>
-        <Box className="flex items-center">
-          <FaPhone size={24} />
-          <span>+1 (408) 896-9528</span>
+        <Box className="flex items-center mb-4">
+          <FaPhone size={48} />
+          <Box>
+            <Typography className="text-xl ml-2">Phone</Typography>
+            <Link href="tel:+14088969528" className="text-xl ml-2 text-blue-400 hover:text-blue-500 hover:underline transition-all duration-300">+1 (408) 896-9528</Link>
+          </Box>
         </Box>
       </Box>
     </Box>
